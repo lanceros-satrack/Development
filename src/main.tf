2c1cd97f7551b9282b53d359991916fc67cf14f0
David Mauricio Gutierrez Ruiz <David_Gutierrez@satrack.com>
2018-10-26 08:46:35 -0500

    CAmbio de Nombre

--- src/main.tf
+++ src/main.tf
@@ -1,5 +1,5 @@
 # Create a resource group
-resource "azurerm_resource_group" {
+resource "azurerm_resource_group" "daviddevops" {
   name     = "${var.resource_group}"
   location = "${var.location}"  
 }
